39442e4409a9820ac8b48c8da4a05b8465f89924
AJ Williams <Ajwilliamsofficial@gmail.com>
2023-12-07 14:35:33 -0800

    search page

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -160,8 +160,8 @@ function setTrack(trackId, newPlaylist, play) {
     $.post("includes/handlers/ajax/getArtistJson.php", { artistId: track.artist }, function(data) {
       var artist = JSON.parse(data);
 
-      $(".artistName span").text(artist.name);
-      $(".artistName span").attr("onclick", "openPage('artist.php?id=" + artist.id + "')");
+      $(".trackInfo .artistName span").text(artist.name);
+      $(".trackInfo .artistName span").attr("onclick", "openPage('artist.php?id=" + artist.id + "')");
     });
 
     $.post("includes/handlers/ajax/getAlbumJson.php", { albumId: track.album }, function(data) {
